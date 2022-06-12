cc9c02c11fb2b1b12559048e214e8836f3f0edf1
MaikJim <maikoljimenez427@gmail.com>
2022-06-12 07:39:34 -0400

    feat: :sparkles: adding Random imagen when reload the page

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 //variables
-const API_PLACEHOLDER_USERS = (id) => `https://jsonplaceholder.typicode.com/users/${id}`;
+const API_PLACEHOLDER_USERS = 'https://jsonplaceholder.typicode.com/users';
 const API_PLACEHOLDER_PHOTO = 'https://jsonplaceholder.typicode.com/photos';
 
 const spanError = document.getElementById('error');
+const random = () => function getRandom(){
+    return Math.floor(Math.random() * (10-1)+1)
+};
 
 //functions
 async function loadRandomPhoto(){
@@ -14,8 +17,25 @@ async function loadRandomPhoto(){
     else {
         const img1 = document.getElementById('img1');
         const img2 = document.getElementById('img2');
-        img1.src = data[0].url;
-        img2.src = data[1].url;
+        img1.src = data[Math.floor(Math.random() * (5000-1)+1)].url;
+        img2.src = data[Math.floor(Math.random() * (5000-2)+2)].url;
     }
 }
 loadRandomPhoto();
+function loadRandomUSers(){
+    fetch(API_PLACEHOLDER_USERS)
+        .then(res => res.json())
+        .then(data => getData(data))
+        .catch(error => console.log(error))
+
+    const getData = (data) => {
+        console.log(data)
+        let body = ""
+        for (var i = 0; i < data.length; i++) {
+            body+=`<tr><td>${data[i].name}</td><td>${data[i].email}</td></tr>`;
+        }
+        document.getElementById('data').innerHTML = body;
+        console.log(body);
+    }
+}
+loadRandomUSers()
